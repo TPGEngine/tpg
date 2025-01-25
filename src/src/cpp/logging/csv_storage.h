15fef58c9d4488f87f71181cc0abf85679b4184d
richardl2003 <richardliy03@gmail.com>
2025-01-25 19:48:56 +0000

    fix: more protections for singleton classes

--- src/src/cpp/logging/csv_storage.h
+++ src/src/cpp/logging/csv_storage.h
@@ -9,7 +9,10 @@ public:
     
     void init(const std::string& filename);
     void append(const std::map<std::string, std::string>& data);
+    CSVStorage(const CSVStorage&) = delete;
+    CSVStorage& operator=(const CSVStorage&) = delete;
 
 private:
+    CSVStorage() = default;
     std::ofstream file_;
 };
--- src/src/cpp/logging/event_dispatcher.h
+++ src/src/cpp/logging/event_dispatcher.h
@@ -11,9 +11,11 @@ public:
     
     static EventDispatcher& instance();
     void subscribe(EventType type, Callback cb);
-
     void notify(EventType type, const std::map<std::string, std::string>& data);
+    EventDispatcher(const EventDispatcher&) = delete;
+    EventDispatcher& operator=(const EventDispatcher&) = delete;
 
 private:
+    EventDispatcher() = default;
     std::map<EventType, std::vector<Callback>> listeners_;
 };
