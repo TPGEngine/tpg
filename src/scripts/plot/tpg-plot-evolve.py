a4a890b2c299d78ae8eed05b05d03687628b98d5
Mark Cruz <markaycruz@gmail.com>
2025-03-09 11:54:20 -0400

    clean up experiments to add nececssary files to `logs/` or `plots/` (#369)
    * Squashed 'src/' changes from 85f74c0..2c8b2c9
    
    2c8b2c9 benchmark inverted double
    8cd6a66 Merge branch 'fix/slurm-script' into 'main'
    a2e20a5 Edit tpg-run-slurm.sh to work with new changes
    19a2d7d fix: remove gitlab ci
    1a7d66e edit README.md
    e181973 Merge branch 'refactor/file-structure' into 'main'
    6472f89 refactor: yaml migration and new tpg cli
    c655a58 add seq cc
    920f294 EvalData.cc
    0e6f632 fix EvalData
    daea48d move EvalData to TPG class
    ed02fe7 move EvalData to TPG class
    3b96797 Merge branch 'fix/cmake-tests' into 'main'
    04d7dd7 fix: updated multitask to work with new generic folder
    a06ac14 Merge branch 'fix/cmake-warnings' into 'main'
    baa9ae5 fix: unused variables in register machine crossover unit tests
    68f0cb2 Merge branch 'feat/logging-mujoco-unit-tests' into 'main'
    504f5cf feat: completed logging, and integrated mujoco unit tests
    18118d0 rm mSz loging
    4a808c7 Merge branch 'feat/logging-unit-tests' into 'main'
    c9bb451 feat: logging, unit test, cmake build, and multitask hyperparams
    173d1dc confirming MuJoco humanoid standup runs
    762b446 mujoco example param tweak
    21e5b6e confirm Mujoco example
    a48491f confirm Mujoco example
    147611a run tests after merge skdev
    5430864 ...
    95cdba9 ...
    92021db ...
    d18b258 Edit README.md
    b43b665 Merge branch 'fix/mpi-script' into 'main'
    b52b828 fix: typo in tpg-run-mpi script
    c0f3645 Merge branch 'feat/dev-containers' into 'main'
    6a4e523 feat: reproducible linux dev environments with dev containers
    92239ab ...
    399ff94 update run script
    fd8e6cc ...
    185a5f8 ...
    839c214 multitask parameter update
    c3d84af fix seed bug in slurm script
    5896872 param tweak
    66986f0 adding classic_control_mutitask
    81d17bb ...
    fec9378 ...
    5c68bbd ...
    cb3a99f ...
    3de4d7a ...
    056462e ...
    0c2cf89 ...
    973c050 ...
    a3171df ...
    48c214b ...
    6800808 ...
    6879529 Merge branch 'skdev' of https://gitlab.cas.mcmaster.ca/kellys32/tpg into skdev
    0d7ba08 ...
    8acabf0 ...
    89dcbc6 ...
    7d4e5bf ...
    2071bba ...
    6848a6c ...
    48cf5ed NYE ops fiasco
    95e9759 ...
    a641b21 ...
    4cd0770 ...
    c55e6a0 ...
    bb48342 ...
    549bf17 ...
    e92608a ...
    72b1070 ...
    b0181ad ...
    ecf83d6 ...
    cc759d5 ...
    aed6b46 ...
    59eadd6 ...
    d98e34d ...
    ee97430 ...
    47db4a0 ...
    cc14488 ...
    4591742 ...
    ddc3cb6 ...
    f44d987 ...
    c0ff8cd ...
    33054a6 ...
    e37b9d4 fix reacher rand init
    7f1a67f ...
    59fc56b ...
    1912d36 ...
    1df3478 ...
    a82aeba ...
    773d142 ...
    281c3b8 ...
    6292fdb ...
    8987026 ...
    97cf5eb ...
    02fd3cc ...
    7427007 ...
    a471338 ...
    e3e3180 ...
    af5d32c ...
    e5fd300 ...
    ded0204 ...
    870dd11 add mujoco ant
    f9b59c8 ...
    0144714 ...
    e49b498 Reacher param update
    32203fc ...
    b4fcdd7 Merge branch 'skdev' into 'main'
    4a12fd1 skdev
    6d2ce0b fix linear xover never-run bug
    a531e9c ...
    5e32b3e Merge branch 'skdev' into 'main'
    6ce879f skdev
    6977258 fix AddProg bug
    d2ac070 ...
    18ba6b0 cleanup and synch skdev
    d1e3eff fix reg crossover
    603c547 fix memory leak and simplify
    8444e08 Merge branch 'skdev'
    1ac20f6 ...
    0c79d65 Merge branch 'skdev'
    bfad028 ...
    a53448d Merge branch 'skdev'
    c3417a6 ...
    17da0ce update reacher params
    59aabd7 ...
    3e8538d ...
    1624352 ...
    1575ea2 ...
    b71b7ea ...
    a206563 ...
    8ff3427 Merge branch 'skdev' into 'main'
    5f3f06e separate mujoco experiment dirs and establish InverterPendulum baseline
    902e747 Merge branch 'new-mujoco-envs' into 'main'
    303a46c New MuJoCo Environments
    
    git-subtree-dir: src
    git-subtree-split: 2c8b2c917159cdcfef7d82f0178de1abc0481608
    
    * move experiment results to `logs/` and `plots/`
    
    * move experiment results to `logs/` and `plots/`
    
    * fix evolve
    
    * fix evolve
    
    * sync files
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/scripts/plot/tpg-plot-evolve.py
+++ src/scripts/plot/tpg-plot-evolve.py
@@ -38,6 +38,9 @@ Example:
     tpg-plot-evolve.py best_fitness
 '''
 
+log_dir = "logs"
+plot_dir = "plots"
+
 def get_unique_filename(base_filename):
     """
     Generates a unique filename by appending an increment if the file already exists.
@@ -49,7 +52,7 @@ def get_unique_filename(base_filename):
     counter = 1
     new_filename = base_filename
     
-    while os.path.exists(new_filename):
+    while os.path.exists(f"{plot_dir}/{new_filename}"):
         new_filename = f"{filename}_{counter}{ext}"
         counter += 1
     
@@ -57,7 +60,7 @@ def get_unique_filename(base_filename):
 
 def get_csv_columns(file):
     """Extracts column names from a CSV file."""
-    with open(file, newline="", encoding="utf-8") as file:
+    with open(f"{log_dir}/{file}", newline="", encoding="utf-8") as file:
         reader = csv.reader(file)
         column_names = next(reader)
         return column_names[1:]
@@ -82,7 +85,7 @@ def plot_generations_single(csv_files, column_name, pdf = None):
     # start processing the listed csv files
     for idx, csv_file in enumerate(csv_files):
         try:
-            df = pd.read_csv(csv_file)
+            df = pd.read_csv(f"{log_dir}/{csv_file}")
             if 'generation' not in df.columns:
                 print(f"Skipping {csv_file}: missing 'generation' column")
                 continue
@@ -107,7 +110,7 @@ def plot_generations_single(csv_files, column_name, pdf = None):
             
         except Exception as e:
             print(f"Error processing {csv_file}: {str(e)}")
-    
+
     if not valid_files:
         print("No valid CSV files with required columns found!")
         return
@@ -135,9 +138,9 @@ def plot_generations_single(csv_files, column_name, pdf = None):
         output_filename += ".pdf"
 
         # if output file already exists, add a number in the end
-        output_filename = get_unique_filename(output_filename)
+        output_filename =  f"{plot_dir}/{get_unique_filename(output_filename)}"
 
-        plt.savefig(output_filename, bbox_inches='tight')
+        plt.savefig(output_filename)
         print(f"Plot saved to '{output_filename}'")
 
     plt.close()
@@ -146,7 +149,10 @@ def plot_generations_multiple(csv_files, column_names):
 
     prefix = csv_files[0].split('.')[0] if csv_files else ""
 
-    output_filename = get_unique_filename(f"{prefix}_all_vs_generations.pdf")
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+
+    output_filename = plot_dir + "/" + get_unique_filename(f"{prefix}_all_vs_generations.pdf")
 
     with PdfPages(output_filename) as pdf:
         for column_name in column_names:
@@ -171,7 +177,11 @@ if __name__ == "__main__":
     if csv_key.startswith("all-"):
         prefix = csv_key[4:]  # extract part after "all-"
         if prefix in prefixes:
-            csv_files = glob.glob(f"{prefix}.*.*.csv")
+            try:
+                os.chdir(log_dir)
+                csv_files = glob.glob(f"{prefix}.*.*.csv")
+            finally:
+                os.chdir("..")
         else:
             raise ValueError(f"Invalid prefix '{prefix}'. Expected one of {prefixes}.")
     else:
@@ -187,7 +197,7 @@ if __name__ == "__main__":
 
     valid_files = []
     for f in csv_files:
-        if not os.path.exists(f):
+        if not os.path.exists(f"{log_dir}/{f}"):
             print(f"Warning: File '{f}' not found")
         else:
             valid_files.append(f)
@@ -207,3 +217,4 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"Error: {str(e)}")
         sys.exit(1)
+        
