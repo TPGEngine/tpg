0b523b54e20ed85d67e79c537bb53277544a1dd3
richardl2003 <richardliy03@gmail.com>
2025-03-14 19:29:46 +0000

    fix: enable localhost to bypass CORS

--- src/src/api/app/main.py
+++ src/src/api/app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from .routers import experiments
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(experiments.router)
 
